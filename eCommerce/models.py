e13e60f47e63abc38d0379e8552fd4e2b8ce9ca5
Jatin Rishi <rishisbn2014@gmail.com>
2021-01-27 15:10:18 +0530

    Small Change

--- eCommerce/models.py
+++ eCommerce/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=15, unique=True)
+    name = models.CharField(max_length=30, unique=True)
 
     class Meta:
         ordering = ['name']
