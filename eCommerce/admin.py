168225da1072ca7930a147505c22370d0d2624d8
Jatin Rishi <rishisbn2014@gmail.com>
2021-01-28 18:11:27 +0530

    Test Changes

--- eCommerce/admin.py
+++ eCommerce/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Item, Category
+from .models import Item, Category,PhotoItem
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Item)
+admin.site.register(PhotoItem)
